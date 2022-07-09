dd5c00c77852c158e22ceaeb049b007a788854bc
BadmWe <dimakekfedotov@gmail.com>
2022-07-09 03:10:09 +0300

    navbar spacing with pages fixed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ import backgroundImage from '@/images/background.jpg'
 export function Hero() {
   return (
     <div className="relative pt-10 pb-20 sm:py-24">
-      <div className="absolute inset-x-0 -top-48 -bottom-14 overflow-hidden bg-indigo-50">
+      <div className="absolute inset-x-0 -bottom-14 overflow-hidden bg-indigo-50">
         <div className="absolute top-0 left-0 translate-y-[-10%] translate-x-[-55%] -scale-x-100 sm:left-1/2 sm:translate-y-[-6%] sm:translate-x-[-98%] lg:translate-x-[-106%] xl:translate-x-[-122%]">
           <Image
             src={backgroundImage}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -21,7 +21,7 @@ const Navbar = () => {
   return (
     <>
       <div className="min-h-full">
-        <div className="bg-gray-800 pb-32">
+        <div className="bg-gray-800">
           <Disclosure as="nav" className="bg-gray-800">
             {({ open }) => (
               <>
@@ -140,9 +140,6 @@ const Navbar = () => {
               </>
             )}
           </Disclosure>
-          <header className="py-10">
-            <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8"></div>
-          </header>
         </div>
       </div>
     </>
