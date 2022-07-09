8b2c64e2582e71fb7f81c188a5641e629f3eb0d8
BadmWe <dimakekfedotov@gmail.com>
2022-07-09 06:53:04 +0300

    changed company name in Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
       <Container className="flex flex-col items-center justify-between md:flex-row">
         <Logo className="h-12 w-auto text-slate-900" />
         <p className="mt-6 text-base text-slate-500 md:mt-0">
-          Copyright &copy; {new Date().getFullYear()} DeceptiConf, LLC. All
+          Copyright &copy; {new Date().getFullYear()} WebBadminton, LLC. All
           rights reserved.
         </p>
       </Container>
