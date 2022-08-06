2e3c81eb1e58d5f9584e5430d4f549dd565c5ad7
BadmWe <dimakekfedotov@gmail.com>
2022-08-06 23:03:24 +0300

    index seo

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,9 +1,24 @@
 import { Hero } from '@/components/Hero'
 import { Newsletter } from '@/components/Newsletter'
-
+import { Head } from 'next/document'
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Web Badminton</title>
+        <meta
+          name="description"
+          content="A platform where we talk about the rules of badminton, brands, equipment and much more."
+        />
+        <link rel="icon" href="/favicon.ico" />
+        <meta property="og:title" content="WebBadminton" />
+        <meta
+          property="og:description"
+          content="A platform where we talk about the rules of badminton, brands, equipment and much more."
+        />
+        <meta property="og:url" content="https://webbadminton.com/" />
+        <meta property="og:type" content="website" />
+      </Head>
       <Hero />
       <Newsletter />
     </>
