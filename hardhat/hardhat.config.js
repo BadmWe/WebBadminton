869d340de5123e69f747a51ae4c0ccdcd885aaf9
pfed-prog <fedorovn19@gmail.com>
2022-08-24 23:59:18 +0200

    updated test

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -1,16 +1,13 @@
 require('@nomicfoundation/hardhat-toolbox')
-
-const ALCHEMY_API_KEY = 'WmSjTjlKqH-UP69pveZ8zrwJljFXJChZ'
-const MUMBAI_PRIVATE_KEY =
-  '57cb47da57d864f5f1b0de38c7e4a2ab12ede9d8b4bd37a68966da0dcdbc7309'
+require('dotenv').config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: '0.8.9',
   networks: {
     mumbai: {
-      url: `https://polygon-mumbai.g.alchemy.com/v2/${ALCHEMY_API_KEY}`,
-      accounts: [MUMBAI_PRIVATE_KEY],
+      url: `https://polygon-mumbai.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      accounts: [process.env.MUMBAI_PRIVATE_KEY],
     },
   },
 }
--- hardhat/test/erc1155.js
+++ hardhat/test/erc1155.js
@@ -1,35 +1,14 @@
 const { expect } = require('chai')
 
 describe('Token contract', function () {
-  it('Should mint Rock', async function () {
+  it('Should mint Item', async function () {
     const accounts = await ethers.getSigners()
 
-    const TinyVillage = await ethers.getContractFactory('MyERC1155NFT')
-    const tinyVillage = await TinyVillage.deploy()
+    const NFTcontract = await ethers.getContractFactory('MyERC1155NFT')
+    const nftcontract = await NFTcontract.deploy()
 
-    await tinyVillage.mintRock()
-    const balance = await tinyVillage.balanceOf(accounts[0].address, 1)
-    expect(1).to.equal(Number(balance.toString()))
-  })
-  it('Should mint 2 paper', async function () {
-    const accounts = await ethers.getSigners()
-
-    const TinyVillage = await ethers.getContractFactory('MyERC1155NFT')
-    const tinyVillage = await TinyVillage.deploy()
-
-    await tinyVillage.mintPaper()
-    await tinyVillage.mintPaper()
-    const balance = await tinyVillage.balanceOf(accounts[0].address, 2)
-    expect(2).to.equal(Number(balance.toString()))
-  })
-  it('Should mint scissors', async function () {
-    const accounts = await ethers.getSigners()
-
-    const TinyVillage = await ethers.getContractFactory('MyERC1155NFT')
-    const tinyVillage = await TinyVillage.deploy()
-
-    await tinyVillage.mintScissors()
-    const balance = await tinyVillage.balanceOf(accounts[0].address, 3)
+    await nftcontract.mintArcsaber11()
+    const balance = await nftcontract.balanceOf(accounts[0].address, 0)
     expect(1).to.equal(Number(balance.toString()))
   })
 })
