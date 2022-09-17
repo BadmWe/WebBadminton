42c9099c801f734aa95009c821f15847580e3476
BadmWe <dimakekfedotov@gmail.com>
2022-09-17 22:38:27 +0300

    added red logo

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -20,7 +20,7 @@ export default function Home() {
         />
         <meta property="og:url" content="https://webbadminton.com/" />
         <meta property="og:type" content="website" />
-        <meta property="og:image" content="https://i.imgur.com/UpwfqYB.jpg" />
+        <meta property="og:image" content="https://i.imgur.com/ARpl2RX.jpg" />
       </Head>
       <Hero />
       <Newsletter />
